5d0ee51db30dccf324090b56132dcc82a62577d4
arkasaharoy23 <149593413+arkasaharoy23@users.noreply.github.com>
2026-09-04 11:46:24 +0530

    fixing

--- server/app.js
+++ server/app.js
@@ -28,7 +28,8 @@ app.use(cors({
     'http://127.0.0.1:5500',
     'https://medisphere-frontend-cbcg.onrender.com'
   ],
-  credentials: true
+  methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"]
 }));
 app.use(express.json());
 
